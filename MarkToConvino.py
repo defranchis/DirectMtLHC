92e09850136030faef47bab201790443c028545e
matteo.defranchis <matteo.defranchis@cern.ch>
2021-04-14 11:38:57 +0200

    common code moved to external file

--- MarkToConvino.py
+++ MarkToConvino.py
@@ -1,5 +1,6 @@
 import sys, os
 import itertools
+from writeConvinoOutput import *
 
 simpleInput = True
 permutations = False
@@ -83,70 +84,6 @@ def getAllInfo(infilename):
         sys.exit()
 
     return [systnames, measurements,all_central,all_uncertainties,all_corr_dict]
-
-def writeConfig(outdir,systnames,measurements):
-    f = open(outdir+'/mt_config.txt','w')
-    f.write('[global]\n\n[end global]\n\n[inputs]\n\n')
-    f.write('\tnFiles = {} \n\n'.format(len(measurements)))
-    for i, meas in enumerate(measurements):
-        f.write('\tfile{} = {}.txt\n'.format(i,meas))
-    f.write('\n[end inputs]\n\n[observables]\n\n\tmt_comb = ')
-    for meas in measurements:
-        f.write('mt_{} '.format(meas))
-        if meas!= measurements[-1]: 
-            f.write('+ ')
-        else: f.write('\n\n[end observables]\n\n')
-    f.write('[correlations]\n\n\t#!FILE  =  extra_correlations.txt\n\n[end correlations]\n\n[uncertainty impacts]\n\n')
-    for syst in systnames:
-        if syst == 'Stat': continue
-        f.write('\t{} = '.format(syst))
-        for meas in measurements:
-            f.write('{}_{} '.format(syst,meas))
-            if meas != measurements[-1]:
-                f.write('+ ')
-            else:
-                f.write('\n')
-    f.write('\n\n[end uncertainty impacts]\n')
-    f.close()
-    return
-
-def writeMeasurementFile(outdir,systnames,measurement,value,uncert):
-    f = open('{}/{}.txt'.format(outdir,measurement),'w')
-    f.write('\n\n[hessian]\n\n[end hessian]\n\n[correlation matrix]\n\n[end correlation matrix]\n\n[not fitted]\n\n\t')
-    systnames.remove('Stat')
-    systnames.append('Stat')
-    for syst in systnames:
-        if syst == 'Stat':
-            f.write('stat ')
-        else:
-            f.write('{}_{} '.format(syst,measurement))
-    f.write('\n\tmt_{} '.format(measurement))
-    for syst in systnames:
-        f.write('{} '.format(uncert[measurement][syst]))
-    f.write('\n\n[end not fitted]\n\n[systematics]\n\n[end systematics]\n\n[estimates]\n\n')
-    f.write('\tn_estimates = 1\n\n')
-    f.write('\tname_0 = mt_{}\n'.format(measurement))
-    f.write('\tvalue_0 = {}\n'.format(value[measurement]))
-    f.write('\n[end estimates]\n')
-    f.close()
-    return
-
-def writeAllFiles(outdir,systnames,measurements,value,uncert):
-    for measurement in measurements:
-        writeMeasurementFile(outdir,systnames,measurement,value,uncert)
-    return
-
-def writeCorrelations(outdir,systnames,measurements,matrix):
-    f = open(outdir+'/extra_correlations.txt','w')
-    for syst in systnames:
-        if syst == 'Stat': continue
-        for i, meas_i in enumerate(measurements):
-            for j, meas_j in enumerate(measurements):
-                if not j>i: continue
-                f.write('{}_{} = ({}) {}_{}\n'.format(syst,meas_i,matrix[syst][meas_i][meas_j],syst,meas_j))
-        f.write('\n')
-    f.close()
-    return
 
 
 infilename = sys.argv[1]
