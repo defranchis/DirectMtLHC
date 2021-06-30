10be2a37762408645ef440511c76a65472c84cd6
matteo.defranchis <matteo.defranchis@cern.ch>
2021-06-30 12:29:14 +0200

    fist working LHC combination

--- mergeCovarianceConvino.py
+++ mergeCovarianceConvino.py
@@ -10,6 +10,8 @@ outdir = 'inputsConvinoLHC'
 if not os.path.exists(outdir):
     os.makedirs(outdir)
 
+corr_d = {'MCGEN': 0.5, 'CR': 1, 'METH': 0, 'RAD': 0.5, 'JES1': 0, 'BKMC': 1, 'JER': 0, 'TRIG': 0, 'PDF': 1, 'BTAG': 0.5, 'PU': 1, 'JES2': 0, 'JES3': 0.5, 'LES': 0, 'JES6': 0, 'JES4': 1, 'JES5': 0.5, 'UE': 1, 'BKDT': 0, 'HADR': 0, 'MET': 0}
+
 def readPostFitInfo(experiment):
     f = open('{}/{}_result.txt'.format(indir,experiment),'r')
     l_orig = f.read().splitlines()
@@ -141,6 +143,18 @@ def normalizeInputsForConvino(cov):
             norm[i,j] /= unc[i]*unc[j]
     return norm
 
+def renameMergedListCMS(mergedlist):
+    l = []
+    for m in mergedlist:
+        if m == 'mt': 
+            l.append(m)
+            continue
+        r = m.upper()
+        r = r.replace('LHC','')
+        if r == 'HAD': r = 'HADR'
+        l.append(r)
+    return l
+
 def mergeInputsForLHC(experiment):
 
     systlist, cov_m, mt_comb  = readPostFitInfo(experiment)
@@ -160,15 +174,88 @@ def mergeInputsForLHC(experiment):
 
     cov_norm = normalizeInputsForConvino(cov_merged)    
     hessian = invertMatrix(cov_norm)
+    if experiment == 'CMS':
+        mergedlist = renameMergedListCMS(mergedlist)
     writeConvinoInputFromCovariance(experiment,hessian, mergedlist, mt_comb)
 
+    return mergedlist
+
+def writeConvinoConfig(experiments, mergedlist_d):
+    f = open('{}/mt_config.txt'.format(outdir),'w')
+
+    f.write('[global]\n\n[end global]\n\n[inputs]\n\n')
+    f.write('\tnFiles = {} \n'.format(len(experiments)))
+    for i, exp in enumerate(experiments):
+        f.write('\tfile{} = allinputs_{}.txt\n'.format(i,exp))
+    f.write('\n[end inputs]\n\n[observables]\n\n\tmt_comb = ')
+    for exp in experiments:
+        f.write('mt_{} '.format(exp))
+        if exp!= experiments[-1]: 
+            f.write('+ ')
+        else: f.write('\n\n[end observables]\n\n')
+    f.write('[correlations]\n\n\t#!FILE  =  extra_correlations.txt\n\n[end correlations]\n\n[uncertainty impacts]\n\n')
+
+    common = getCommonSyst(mergedlist_d)
+    
+    for c in common:
+        f.write('\t{} = '.format(c))
+        first = True
+        for exp in experiments:
+            if first:
+                f.write('{}_{} '.format(c,exp))
+                first = False
+            else:
+                f.write('+ {}_{} '.format(c,exp))
+        f.write('\n')
+    f.write('\n')
+
+    for exp in experiments:
+        l = mergedlist_d[exp]
+        for ll in l:
+            if ll == 'mt':
+                continue
+            if not ll in common:
+                f.write('\t{} = {}_{}\n'.format(ll,ll,exp))
+        f.write('\n')
+    f.write('\n[end uncertainty impacts]\n')
+
+    f.close()
     return
 
+def getCommonSyst(mergedlist_d):
+    common = []
+    experiments = mergedlist_d.keys()
+    onelist = mergedlist_d[experiments[0]]
+    for s in onelist:
+        if s == 'mt': continue
+        iscommon = True
+        for i in range(1,len(experiments)):
+            if not s in mergedlist_d[experiments[i]]:
+                iscommon = False
+                break
+        if iscommon:
+            common.append(s)
+    return common
+
+def writeExternalCorrelations(common):
+    f = open(outdir+'/extra_correlations.txt','w')
+    for c in common:
+        if corr_d[c] != 0:
+            f.write('{}_ATLAS = ({}) {}_CMS\n'.format(c,corr_d[c],c))
+    f.close()
+    return
 
 def main():
-    for experiment in ['ATLAS','CMS']:
-        print '\n\n-> processing {} inputs \n'.format(experiment)
-        mergeInputsForLHC(experiment)
+    experiments = ['ATLAS','CMS']
+    mergedlist_d = dict()
+    for exp in experiments:
+        print '\n\n-> processing {} inputs \n'.format(exp)
+        ml = mergeInputsForLHC(exp)
+        mergedlist_d[exp] = ml
+
+    writeConvinoConfig(experiments,mergedlist_d)   
+    writeExternalCorrelations(getCommonSyst(mergedlist_d))
+
     return
 
 if __name__ == "__main__":
