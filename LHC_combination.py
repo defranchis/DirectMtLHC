54e287b1b208a38c415ef918ae888826f044fe78
matteo.defranchis <matteo.defranchis@cern.ch>
2022-04-08 17:19:52 +0200

    Revert "merge syst in LHC impact table"
    This reverts commit cab6be40a01d417b24019efc0fea0fbf98a483ef.

--- LHC_combination.py
+++ LHC_combination.py
@@ -8,8 +8,6 @@ import argparse, sys, copy
 f_ATLAS = 'original_inputs/ATLAS_signed_2021_12_14.txt'
 f_CMS = 'original_inputs/CMS_negCorr_CMSGrid_V5_Dec.txt'
 
-mergeImpacts = {'LHCHAD':['HADR','SLEPB','BFRAG']}
-
 def makeLHC_MCstat_file(obj_ATLAS,obj_CMS):
     systForToys = copy.deepcopy(obj_ATLAS.systForToys)
     for syst in obj_CMS.systForToys:
@@ -58,16 +56,16 @@ def main():
     # obj_CMS.printImpactsSorted()
 
     if not args.unblind:
-        LHC_full = LHC_object(obj_ATLAS, obj_CMS, blind=True, separateCombinations=False, mergeImpacts=mergeImpacts)
+        LHC_full = LHC_object(obj_ATLAS, obj_CMS, blind=True, separateCombinations=False)
         LHC_full.printResults()
         LHC_full.printImpactsSorted()
 
-        LHC_sep = LHC_object(obj_ATLAS, obj_CMS, blind=True, separateCombinations=True, mergeImpacts=mergeImpacts)
+        LHC_sep = LHC_object(obj_ATLAS, obj_CMS, blind=True, separateCombinations=True)
         LHC_sep.printResults()
         LHC_sep.printImpactsSorted()
     
-    LHC_full_unblind = LHC_object(obj_ATLAS, obj_CMS, blind=False, separateCombinations=False, mergeImpacts=mergeImpacts)
-    LHC_sep_unblind = LHC_object(obj_ATLAS, obj_CMS, blind=False, separateCombinations=True, mergeImpacts=mergeImpacts)
+    LHC_full_unblind = LHC_object(obj_ATLAS, obj_CMS, blind=False, separateCombinations=False)
+    LHC_sep_unblind = LHC_object(obj_ATLAS, obj_CMS, blind=False, separateCombinations=True)
 
     if args.unblind:
         LHC_full_unblind.printResults()
