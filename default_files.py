052b0b719aa49284160bfcb33fa1eabd57e00cfb
matteo.defranchis <matteo.defranchis@cern.ch>
2022-05-24 17:52:09 +0200

    CMS stat correlations

--- default_files.py
+++ default_files.py
@@ -1,3 +1,3 @@
 
 default_file_ATLAS = 'inputs/ATLASallinputs_2022_05_05.txt'
-default_file_CMS = 'inputs/CMS_negCorr_CMSGrid_V5_Dec.txt'
+default_file_CMS = 'inputs/CMS_negCorr_CMSGrid_StatCorr_2022_05_24.txt'
