c53e1746f949c99e088ae5f5e8a96cf9bd452123
matteo.defranchis <matteo.defranchis@cern.ch>
2022-08-22 14:53:19 +0200

    new ATLAS input file

--- default_files.py
+++ default_files.py
@@ -1,3 +1,3 @@
 
-default_file_ATLAS = 'inputs/ATLASallinputs_2022_05_05.txt'
+default_file_ATLAS = 'inputs/ATLASallinputs_2022_08_19.txt'
 default_file_CMS = 'inputs/CMS_negCorr_CMSGrid_V5_Dec.txt'
