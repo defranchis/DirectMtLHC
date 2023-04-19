cb970e8a4f14eeb4ae3eccb15f97ccaafa62848d
Matteo Defranchis <matteo.defranchis@cern.ch>
2023-04-19 17:05:03 +0200

    ATLAS summary table

--- ATLAS_combination.py
+++ ATLAS_combination.py
@@ -4,6 +4,8 @@ import argparse
 
 import default_files
 
+from LHC_object import mergeMap_default, renameMap_default
+
 infile = default_files.default_file_ATLAS
 MC_file = 'MCstat_ATLAS.txt'
 
@@ -34,8 +36,13 @@ def main():
     args = parser.parse_args()
 
     base_obj = BLUE_object(infile,ATLAS=True,PU_hack=False)
+    for old, new in renameMap_default['ATLAS'].items():
+        base_obj.renameSyst(old,new)
+
+
     base_obj.printResults()
     base_obj.printImpactsSorted()
+    base_obj.printSummaryTable()
 
 
     if args.nToys > 0:
